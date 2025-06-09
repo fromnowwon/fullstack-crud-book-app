f7dd9bfa850d6adc71c603186a8682781ddabd22
Chaewon Jeong <fromnowwon@gmail.com>
2025-06-10 01:16:55 +0900

    feat: client 초기 세팅 및 라우팅 설정

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Books from "./pages/Books";
+import Add from "./pages/Add";
+import Update from "./pages/Update";
+
+function App() {
+  return (
+    <main>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/update" element={<Update />} />
+        </Routes>
+      </Router>
+    </main>
+  );
+}
+
+export default App;

74c45b40fec310837e4f150ffd230514bd1faf95
Chaewon Jeong <fromnowwon@gmail.com>
2025-06-10 01:49:14 +0900

    feat: add 기능 추가

--- client/src/pages/Add.tsx
+++ client/src/pages/Add.tsx
@@ -1,3 +1,68 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Add() {
-  return <div>Add</div>;
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    cover: "",
+    price: null,
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBook((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    try {
+      await fetch("http://localhost:8800/books", {
+        method: "POST",
+        body: JSON.stringify(book),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      navigate("/");
+    } catch (error) {
+      console.error("Failed to add book:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Add New Book</h1>
+      <input
+        type="text"
+        name="title"
+        placeholder="title"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="desc"
+        placeholder="desc"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="cover"
+        placeholder="cover"
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        name="price"
+        placeholder="price"
+        onChange={handleChange}
+      />
+      <button onClick={handleClick}>Add Book</button>
+    </div>
+  );
 }

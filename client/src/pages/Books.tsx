936a82042bd98d6eed84668384f3296dde325a57
Chaewon Jeong <fromnowwon@gmail.com>
2025-06-10 01:37:25 +0900

    feat: book 페이지 및 book 데이터 타입 추가

--- client/src/pages/Books.tsx
+++ client/src/pages/Books.tsx
@@ -1,3 +1,52 @@
+import { useEffect, useState } from "react";
+import type { Book } from "../types/book";
+import { Link } from "react-router-dom";
+
 export default function Books() {
-  return <div>Books</div>;
+  const [books, setBooks] = useState<Book[]>([]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await fetch("http://localhost:8800/books");
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+        setBooks(data);
+      } catch (error) {
+        console.error("Failed to fetch books:", error);
+      }
+    };
+
+    fetchBooks();
+  }, []);
+
+  return (
+    <div>
+      <h1>Book List</h1>
+      <div>
+        {books.length > 0 ? (
+          books.map((book) => (
+            <div key={book.id}>
+              <h2>{book.title}</h2>
+              <p>Desc: {book.desc}</p>
+              <p>cover: {book.cover}</p>
+              <span>{book.price}</span>
+            </div>
+          ))
+        ) : (
+          <p>No books available</p>
+        )}
+      </div>
+
+      <button>
+        <Link to="/add">Add</Link>
+      </button>
+    </div>
+  );
 }

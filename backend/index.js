8a7196df09f82f174bab3f29d30af1b934b0b45b
Chaewon Jeong <fromnowwon@gmail.com>
2025-06-10 01:03:03 +0900

    feat: db 연결

---
+++ backend/index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import mysql from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("MySQL 연결 오류:", err);
+    return;
+  }
+  console.log("MySQL 연결 성공");
+});
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("Hello from backend");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books(`title`, `desc`, `cover`) VALUES (?)";
+  const values = [req.body.title, req.body.desc, req.body.cover];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been created successfully.");
+  });
+});
+
+app.listen(8800, () => {
+  console.log("Backend server is running on port 8800");
+});
